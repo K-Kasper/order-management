8df0c8d6e287351a358bde256e143c7cc2cabcf5
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2025-06-16 21:20:48 +0300

    feat: initialize Spring Boot application

---
+++ src/test/java/com/kerajarvi/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerajarvi.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
