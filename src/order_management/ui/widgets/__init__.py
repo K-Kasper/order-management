e850c948b4a3f9840a9946eaf295b0e6da81bd4c
Kasper Kerajärvi <kasperkerajarvi@gmail.com>
2026-05-18 23:07:13 +0300

    refactor: remove unused form fields

--- src/order_management/ui/widgets/__init__.py
+++ src/order_management/ui/widgets/__init__.py
@@ -1,12 +1,6 @@
 """Reusable UI widget components."""
 
 from order_management.ui.widgets.button_bar import ButtonBar
-from order_management.ui.widgets.form_fields import (
-    LabeledCombobox,
-    LabeledDateEntry,
-    LabeledEntry,
-    LabeledText,
-)
 from order_management.ui.widgets.treeview_helper import (
     clear_treeview,
     configure_treeview_tags,
@@ -14,10 +8,6 @@ from order_management.ui.widgets.treeview_helper import (
 
 __all__ = [
     "ButtonBar",
-    "LabeledCombobox",
-    "LabeledDateEntry",
-    "LabeledEntry",
-    "LabeledText",
     "clear_treeview",
     "configure_treeview_tags",
 ]
